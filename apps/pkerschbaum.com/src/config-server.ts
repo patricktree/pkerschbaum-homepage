4570bbdfa523dbb716438fcdeef4b3de4aae7b39
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2025-11-07 14:34:59 +0100

    chore(pkerschbaum.com): remove collection of rrweb events again

--- apps/pkerschbaum.com/src/config-server.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable n/no-process-env -- configs are the only place where reading from process.env is allowed */
-import 'server-only';
-
-export const configServer = {
-  neonDatabaseUrl: process.env.DATABASE_URL,
-};
